bde8073e2fa20296e36067035a4a17946e31d667
Roger Pérez <xruchi99x@gmail.com>
2018-10-28 14:11:46 +0100

    Create deberes2.py

---
+++ deberes2.py
@@ -0,0 +1,9 @@
+#Els jubilats no paguen
+#Els homes no jubilats paguen 1€
+#Les dones no jubilades rosses no paguen
+#Les altres paguen 0.5€
+
+edad=int(input("¿Quina edat tens?")
+sexe=input("¿Quin es el teu sexe?")
+cabells=input("¿De quin color tens els cabells?")
+if eda
