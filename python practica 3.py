a276d2b83868d70c077152cc0beb87f2c99729e3
Roger Pérez <xruchi99x@gmail.com>
2018-10-28 12:53:02 +0100

    Update python practica 3.py

--- python practica 3.py
+++ python practica 3.py
@@ -1,15 +1,15 @@
 dia_semana=input("¿En que dia de la semana estamos?")
 clima=input("¿Cual es el clima actual?")
 estacion=input("¿En que estacion del año estamos?")
-hora=int(input("¿Que hora es?")
-temperatura=int(input("¿a cuantos grados estamos?"))
+hora=int(input("¿Que hora es?"))
+temperatura=int(input("¿A cuantos grados estamos?"))
 if(dia_semana=="Domingo"):
     print("Ponte el reloj de los domingos")
 if(clima=="lluvia"):
     print("Ponte el sombrero")
 if(clima=="soleado")and((hora>=13)and(hora<=16)):
     print("Ponte las gafas de sol")
-if(estacion="verano")and((hora>=22)and(hora=>=24)or(hora>=00)and(hora<=04))and(temperatura<=17):
+if((estacion="verano")and(((hora>=22)and(hora<=24))or((hora>=00)and(hora<=04)))and(temperatura<=17)):
     print("Ponte un pantalón largo")
     else:
         print("Ponte pantalon corto")
