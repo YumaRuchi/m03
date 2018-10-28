d16601956d35f4167f7ba678593be5bbd8521e81
Roger Pérez <xruchi99x@gmail.com>
2018-10-28 14:21:06 +0100

    Update deberes2.py

--- deberes2.py
+++ deberes2.py
@@ -3,7 +3,12 @@
 #Les dones no jubilades rosses no paguen
 #Les altres paguen 0.5€
 
-edad=int(input("¿Quina edat tens?")
+edad=int(input("¿Quina edat tens?"))
 sexe=input("¿Quin es el teu sexe?")
 cabells=input("¿De quin color tens els cabells?")
-if eda
+if (edad>=65) or ((sexe="dona") and (edad<65) and (cabells="rossos")):
+         print("No pagas")
+elif (sexe="home") and (edad<65):
+         print("pagas 1€)
+else:
+         print("pagas 0.2€")
