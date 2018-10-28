926825e83388db3c2ebcafbbe3e9621956aca8aa
Roger Pérez <xruchi99x@gmail.com>
2018-10-28 14:44:01 +0100

    Update deberes2.py

--- deberes2.py
+++ deberes2.py
@@ -6,9 +6,9 @@
 edad=int(input("¿Quina edat tens?"))
 sexe=input("¿Quin es el teu sexe?")
 cabells=input("¿De quin color tens els cabells?")
-if (edad>=65) or ((sexe="dona") and (edad<65) and (cabells="rossos")):
+if (edad>=65) or ((sexe=="dona") and (edad<65) and (cabells=="rossos")):
          print("No pagas")
-elif (sexe="home") and (edad<65):
-         print("pagas 1€)
+elif (sexe=="home") and (edad<65):
+         print("pagas 1€")
 else:
          print("pagas 0.2€")
