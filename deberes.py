85aeba8e421be8ff963106a53fc56e6fd491385f
Roger Pérez <xruchi99x@gmail.com>
2018-10-28 14:07:42 +0100

    Update deberes.py

--- deberes.py
+++ deberes.py
@@ -1,4 +1,8 @@
-<5 - Gratis
->= 65 - Gratis
-resto - 2.15€
-edad=int(input("¿Que edad tienes?")
+#<5 - Gratis
+#>= 65 - Gratis
+#resto - 2.15€
+edad=int(input("¿Que edad tienes?"))
+if edad<5 or edad>=65:
+         print("Es gratis")
+else:
+         print("Pagas 2.15€")
