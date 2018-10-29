a614b9b31eb72412a5d729f570ed33d036dcdc7d
Roger Pérez <xruchi99x@gmail.com>
2018-10-29 21:02:06 +0100

    Update python practica 1.py

--- python practica 1.py
+++ python practica 1.py
@@ -1,5 +1,5 @@
 humedad = int(input("Humedad actual: "))
 if humedad >= 80 :
-print ("Llevate el paraguas!!!")
+    print ("Llevate el paraguas!!!")
 else:
-print ("No te hace falta el paraguas")
+    print ("No te hace falta el paraguas")
