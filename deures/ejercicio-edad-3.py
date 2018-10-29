a65015e6085248d1e8f2faf2b04f16926e770a2d
Roger Pérez <xruchi99x@gmail.com>
2018-10-29 21:00:22 +0100

    Update ejercicio-edad-3.py

--- deures/ejercicio-edad-3.py
+++ deures/ejercicio-edad-3.py
@@ -4,3 +4,8 @@
 #-O 17
 #le decimos que puede entrar en la sesión de jóvenes.
 #Pista: usar paréntesis ()
+edad=int(input("¿Que edad tienes?"))
+if (edad>=18 and edad<=23) or edad==17:
+    print("Puedes entrar a la sesión de jóvenes")
+else:
+    print("No puedes entrar a la sesión de jóvenes")
