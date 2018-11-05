d0bff818cfd215d8029f8bd5e9b036ffc34fbea5
Roger Pérez <xruchi99x@gmail.com>
2018-11-05 15:23:11 +0100

    Create examen.py

---
+++ deures/examen.py
@@ -0,0 +1,20 @@
+loteria=input("Has ganado la loteria?")
+if loteria=="si":
+	print("Cobras 3.000 euros netos")
+else:
+	me_caso=input("¿Te quieres casar con ese milloneti?")
+	if me_caso=="si":
+		edad=int(input("¿Que edad tiene?"))
+		problemas_corazon=input("¿Tiene problemas del corazón?")
+		casado=input("¿Está casado?")
+	if edad>=80 and problemas_corazon=="si" and casado=="no":
+		print("Estás de suerte, sueldo vitalicio")
+	else:
+		print("Todavía tienes una forma de cobrar 3.000€ al mes, a ver esas notas.")
+		m01=int(input("¿Que nota sacaste en m01?"))
+		m02=int(input("¿Que nota sacaste en m02?"))
+		m03=int(input("¿Que nota sacaste en m03?"))
+		m05=int(input("¿Que nota sacaste en m05?"))
+		if m01>=9 and m02>=10 and m03>=8 and (m05>=6 and m05<=8):
+			if m01*0.30+m01*0.40+m01*0.25+m01*0.05>8:
+				print("tu jefe te va a pagar 3.000€ netos al mes")
