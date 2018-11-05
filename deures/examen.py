ea9bed71b61c7cac8fa64f1d1bb41f3c13481edd
Roger Pérez <xruchi99x@gmail.com>
2018-11-05 15:29:24 +0100

    Update examen.py

--- deures/examen.py
+++ deures/examen.py
@@ -8,7 +8,7 @@ else:
         problemas_corazon=input("¿Tiene problemas del corazón?")
         casado=input("¿Está casado?")
         if edad>=80 and problemas_corazon=="si" and casado=="no":
-            print("Estás de suerte, sueldo vitalicio")
+            print("Estás de suerte, sueldo vitalicio en cuanto el viejo se muera")
     else:
         print("Todavía tienes una forma de cobrar 3.000€ al mes, a ver esas notas.")
         m01=int(input("¿Que nota sacaste en m01?"))
