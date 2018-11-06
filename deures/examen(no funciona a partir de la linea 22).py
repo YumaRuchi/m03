37d5ebe662dabb0af3bdecb6f1eaae8ee49cc61c
Roger Pérez <xruchi99x@gmail.com>
2018-11-06 15:34:18 +0100

    FUNCIONA SOY EL MEJOR QUIERO COBRAR 30000000000 EUROS AL DIA

--- deures/examen(no funciona a partir de la linea 22).py
+++ deures/examen(no funciona a partir de la linea 22).py
@@ -9,14 +9,14 @@ else:
         casado=input("¿Está casado?")
         if edad>=80 and problemas_corazon=="si" and casado=="no":
             print("Estás de suerte, sueldo vitalicio en cuanto el viejo se muera")
-    else:
-        print("Todavía tienes una forma de cobrar 3.000€ al mes, a ver esas notas.")
-        m01=int(input("¿Que nota sacaste en m01?"))
-        m02=int(input("¿Que nota sacaste en m02?"))
-        m03=int(input("¿Que nota sacaste en m03?"))
-        m05=int(input("¿Que nota sacaste en m05?"))
-        if m01>=9 and m02>=10 and m03>=8 and (m05>=6 and m05<=8):
-            if m01*0.30+m02*0.40+m03*0.25+m05*0.05>8:
-                print("tu jefe te va a pagar 3.000€ netos al mes")
         else:
-            print("Lo siento, tus notas no te van a hacer ganar ese dinero.")
+            print("Todavía tienes una forma de cobrar 3.000€ al mes, a ver esas notas.")
+            m01=int(input("¿Que nota sacaste en m01?"))
+            m02=int(input("¿Que nota sacaste en m02?"))
+            m03=int(input("¿Que nota sacaste en m03?"))
+            m05=int(input("¿Que nota sacaste en m05?"))
+            if m01>=9 and m02>=10 and m03>=8 and (m05>=6 and m05<=8):
+                if m01*0.30+m02*0.40+m03*0.25+m05*0.05>8:
+                    print("tu jefe te va a pagar 3.000€ netos al mes")
+            else:
+                print("Lo siento, tus notas no te van a hacer ganar ese dinero.")
