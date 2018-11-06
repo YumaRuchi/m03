27390ef0f8c71648492461816067d9b9c642b3ed
Roger Pérez <xruchi99x@gmail.com>
2018-11-06 10:24:18 +0100

    Update examen.py

--- deures/examen.py
+++ deures/examen.py
@@ -16,7 +16,7 @@ else:
         m03=int(input("¿Que nota sacaste en m03?"))
         m05=int(input("¿Que nota sacaste en m05?"))
         if m01>=9 and m02>=10 and m03>=8 and (m05>=6 and m05<=8):
-            if m01*0.30+m01*0.40+m01*0.25+m01*0.05>8:
+            if m01*0.30+m02*0.40+m03*0.25+m05*0.05>8:
                 print("tu jefe te va a pagar 3.000€ netos al mes")
         else:
             print("Lo siento, tus notas no te van a hacer ganar ese dinero.")
