51e3eeb85bdc20fc8b134acec87eb454e961b1f7
Roger Pérez <xruchi99x@gmail.com>
2018-11-13 13:11:29 +0100

    Update calculo-areas.py

--- pagas-extra/calculo-areas.py
+++ pagas-extra/calculo-areas.py
@@ -5,7 +5,7 @@ if pregunta_inicial=="T" or pregunta_inicial=="C":
         altura=float(input("¿Cual es la altura del triangulo?"))
         print("Un triangulo de base",base,"y altura",altura,"tiene un area de",base*altura/2,".")
     else:
-        radio=float(input("¿Cual es el area del circulo?"))
-        print("Un circulo de radio",radio,"tiene un area de",3.141592*(radio*radio),".")
+        radio=float(input("¿Cual es el radio del circulo?"))
+        print("Un circulo de radio",radio,"tiene un area de",3.141592*(radio**2),".")
 else:
     print("No has introducido una opción válida.")
