bb252fbe325b3db0268e6034420ea4f4617dc519
Roger Pérez <xruchi99x@gmail.com>
2018-11-13 13:09:20 +0100

    Update calculo-areas.py

--- pagas-extra/calculo-areas.py
+++ pagas-extra/calculo-areas.py
@@ -1,8 +1,11 @@
 pregunta_inicial=input("¿Quieres calcular el area de un triangulo o la de un circulo? (escribe T o C)")
-if pregunta_inicial=="T":
-    base=float(input("¿Cual es la base del triangulo?"))
-    altura=float(input("¿Cual es la altura del triangulo?"))
-    print("Un triangulo de base",base,"y altura",altura,"tiene un area de",base*altura/2,".")
+if pregunta_inicial=="T" or pregunta_inicial=="C":
+    if pregunta_inicial=="T":
+        base=float(input("¿Cual es la base del triangulo?"))
+        altura=float(input("¿Cual es la altura del triangulo?"))
+        print("Un triangulo de base",base,"y altura",altura,"tiene un area de",base*altura/2,".")
+    else:
+        radio=float(input("¿Cual es el area del circulo?"))
+        print("Un circulo de radio",radio,"tiene un area de",3.141592*(radio*radio),".")
 else:
-    radio=float(input("¿Cual es el area del circulo?"))
-    print("Un circulo de radio",radio,"tiene un area de",3.141592*(radio*radio),".")
+    print("No has introducido una opción válida.")
