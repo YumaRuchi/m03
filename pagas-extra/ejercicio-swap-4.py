086d338afecb0d9ba6c4d000f1a2369d4f36564c
Roger Pérez <xruchi99x@gmail.com>
2018-11-15 10:42:18 +0100

    Create ejercicio-swap-4.py

---
+++ pagas-extra/ejercicio-swap-4.py
@@ -0,0 +1,11 @@
+cajon1="movil"
+cajon2="bocadillo"
+cajon3="boli"
+cajon4="bebida"
+cajonswap=cajon3
+cajon3=cajon1
+cajon1=cajonswap
+cajonswap=cajon2
+cajon2=cajon4
+cajon4=cajonswap
+print("en el cajon 1 hay 1",cajon1,"en el cajon 2 hay una",cajon2,"en el cajon 3 hay un",cajon3,"y en el cajon 4 hay un",cajon4,"")
