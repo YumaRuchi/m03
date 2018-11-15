a29364a7c052f28ffba141a0f5af430d638c17e9
Roger Pérez <xruchi99x@gmail.com>
2018-11-15 10:36:27 +0100

    Create ejercicio-swap.py

---
+++ pagas-extra/ejercicio-swap.py
@@ -0,0 +1,6 @@
+mano_der="movil"
+mano_izq="bocadillo"
+mano_swap=mano_der
+mano_der=mano_izq
+mano_izq=mano_swap
+print("tienes un",mano_izq,"en la mano izquierda y un",mano_der,"en la mano derecha.")
