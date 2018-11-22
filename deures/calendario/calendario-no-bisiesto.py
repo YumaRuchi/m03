dda4400e4c072a12bbbd32d41ff54d333e522e41
Roger Pérez <xruchi99x@gmail.com>
2018-11-22 16:25:04 +0100

    Create calendario-no-bisiesto.py

---
+++ deures/calendario/calendario-no-bisiesto.py
@@ -0,0 +1,25 @@
+print("enero")
+for dia in range(0,365):
+    print(dia+1)
+    if dia%365==31-1:
+        print("febrero")
+    if dia%365==59-1:
+        print("marzo")
+    if dia%365==90-1:
+        print("abril")
+    if dia%365==120-1:
+        print("mayo")
+    if dia%365==151-1:
+        print("junio")
+    if dia%365==181-1:
+        print("julio")
+    if dia%365==212-1:
+        print("agosto")
+    if dia%365==242-1:
+        print("septiembre")
+    if dia%365==273-1:
+        print("octubre")
+    if dia%365==303-1:
+        print("novembre")
+    if dia%365==334-1:
+        print("desembre")
