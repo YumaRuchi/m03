cd23454c53f4d3883677793f5c561fb57be6fa2f
Roger Pérez <xruchi99x@gmail.com>
2018-11-28 19:08:33 +0100

    Update condicion-chunga.py

--- python/deures/condicion-chunga/condicion-chunga.py
+++ python/deures/condicion-chunga/condicion-chunga.py
@@ -5,4 +5,16 @@
 #-negativo
 #Nota: una sola condició
 numero=float(input("Escribe un numero:"))
-if  numero < 0
+if numero>=-10 and numero<=40:
+    pos="Está entre el -10 y el 40"
+else:
+    pos="No está entre el -10 y el 40"
+if numero%2==0:
+    PI="Es par"
+else:
+    PI="Es inpar"
+if numero<0:
+    NP="Es negativo"
+else:
+    NP="Es positivo"
+print("Datos sobre el número:\n-",pos,"\n-",PI,"\n-",NP,)
